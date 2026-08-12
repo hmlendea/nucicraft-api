3ca339ddfba9389b8fd771a01269062b847d2bfd
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-08-12 18:39:42 +0300

    players: Support Player GET for each identifier

--- NuciCraft.API.UnitTests/Controllers/PlayersControllerTests.cs
+++ NuciCraft.API.UnitTests/Controllers/PlayersControllerTests.cs
@@ -12,6 +12,54 @@ namespace NuciCraft.API.UnitTests.Controllers
     public sealed class PlayersControllerTests
     {
         [Test]
+        public void GivenTheGetAction_WhenInspectingItsRoute_ThenItUsesTheIdentifierSegment()
+        {
+            MethodInfo actionMethod = typeof(PlayersController)
+                .GetMethod(nameof(PlayersController.Get), [typeof(string)]);
+            RouteAttribute routeAttribute = actionMethod
+                .GetCustomAttribute<RouteAttribute>();
+
+            Assert.That(routeAttribute, Is.Not.Null);
+            Assert.That(routeAttribute.Template, Is.EqualTo("{playerIdentifier}"));
+        }
+
+        [Test]
+        public void GivenTheGetByUsernameAction_WhenInspectingItsRoute_ThenItUsesTheByUsernameSegment()
+        {
+            MethodInfo actionMethod = typeof(PlayersController)
+                .GetMethod(nameof(PlayersController.GetByUsername));
+            RouteAttribute routeAttribute = actionMethod
+                .GetCustomAttribute<RouteAttribute>();
+
+            Assert.That(routeAttribute, Is.Not.Null);
+            Assert.That(routeAttribute.Template, Is.EqualTo("by-username/{username}"));
+        }
+
+        [Test]
+        public void GivenTheGetByOfflineUuidAction_WhenInspectingItsRoute_ThenItUsesTheByOfflineUuidSegment()
+        {
+            MethodInfo actionMethod = typeof(PlayersController)
+                .GetMethod(nameof(PlayersController.GetByOfflineUuid));
+            RouteAttribute routeAttribute = actionMethod
+                .GetCustomAttribute<RouteAttribute>();
+
+            Assert.That(routeAttribute, Is.Not.Null);
+            Assert.That(routeAttribute.Template, Is.EqualTo("by-offline-uuid/{offlineUUID}"));
+        }
+
+        [Test]
+        public void GivenTheGetByOnlineUuidAction_WhenInspectingItsRoute_ThenItUsesTheByOnlineUuidSegment()
+        {
+            MethodInfo actionMethod = typeof(PlayersController)
+                .GetMethod(nameof(PlayersController.GetByOnlineUuid));
+            RouteAttribute routeAttribute = actionMethod
+                .GetCustomAttribute<RouteAttribute>();
+
+            Assert.That(routeAttribute, Is.Not.Null);
+            Assert.That(routeAttribute.Template, Is.EqualTo("by-online-uuid/{onlineUUID}"));
+        }
+
+        [Test]
         public void GivenThePatchByIdentifierAction_WhenInspectingItsRoute_ThenItUsesTheIdentifierSegment()
         {
             MethodInfo actionMethod = typeof(PlayersController)
